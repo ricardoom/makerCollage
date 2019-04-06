4d7c0c6639e9e5184ba34543357a5caf542aa886
Ricardo E. Galvez <galvez667@gmail.com>
2019-04-06 09:20:42 -0400

    :meat_on_bone: working through the single image

--- index.js
+++ index.js
@@ -6,25 +6,73 @@
 
 // DC Authentication Token
 
-//  j6od0lgm6qprob3s
+//
 
 // place images on screen
-let imageUno = document.getElementById('firstImage');
-function getDCImage(api) {
-  function reqListener() {
-    console.log(this.responseText);
-  }
+const imageContainer = document.getElementById('firstImage');
 
-  let dc = new XMLHttpRequest();
-  dc.addEventListener('load', reqListener);
-  dc.open('GET', api, true);
-  dc.setRequestHeader('Access-Control-Allow-Origin', '*');
-  dc.setRequestHeader('Authorization', 'Token');
-  dc.send();
+const buttonOne = document.getElementById('buttonOne');
+
+const imageUno = document.getElementById('firstImage');
+
+const inputOne = document.getElementById('inputFieldOne');
+
+// let loremFlickrJSON = 'https://loremflickr.com/json/640/480/';
+
+let loremFlickr = 'https://loremflickr.com/640/480/';
+
+let loremWord = 'paris';
+
+let loremRando = '?random=1';
+
+let jsonPlaceholder = 'https://jsonplaceholder.typicode.com/users';
+
+// get the word from the input field
+
+function getTheLoremImage() {
+  let theInputValue = inputOne.value;
+  let newImageURL = `${loremFlickr}${theInputValue}${loremRando}`;
+  return newImageURL;
 }
-let dcURL =
-  'http://api.repo.nypl.org/api/v1/items/search?q=cats&publicDomainOnly=true';
-getDCImage(dcURL);
+
+function updateImgURL() {
+  let newURL = getTheLoremImage();
+  imageContainer.src = newURL;
+}
+
+buttonOne.addEventListener('click', updateImgURL);
+
+// SANDBOX
+
+// const dcToken = 'j6od0lgm6qprob3s';
+// let dcURL =
+//   'http://api.repo.nypl.org/api/v1/items/search?q=cats&publicDomainOnly=true';
+
+// function learnJson() {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+//       let output = '<h2>Users</h2>';
+//       data.forEach(function(user) {
+//         output += `
+//           <ul>
+//             <li>${user.name}</li>
+//             <li>${user.company.bs}</li>
+//           </ul>
+//         `;
+//       });
+//       document.getElementById('output').innerHTML = output;
+//     });
+// }
+
+// learnJson();
+
+// .then(function(myJSON) {
+//   console.log(JSON.stringify(myJSON));
+// });
+
+// getDCImage(dcURL);
+
 // use an svg as the mask
 // get svgs from set of premade svgs
 
