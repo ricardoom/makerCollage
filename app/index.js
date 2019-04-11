8091d8b72b93f50b1b0e8290dcc4f76ad31011c1
Ricardo E. Galvez <galvez667@gmail.com>
2019-04-11 10:05:26 -0400

    :meat_on_bone: added browser sync and all its node modules futzed w/ the app as well

---
+++ app/index.js
@@ -0,0 +1,63 @@
+// collage maker
+
+// place images on screen
+const imageContainer = document.getElementById('firstImage');
+
+const buttonOne = document.getElementById('buttonOne');
+
+const inputOne = document.getElementById('inputFieldOne');
+
+const loader = '<span>Loading Cabron...</span>';
+
+let loremFlickr = 'https://loremflickr.com/640/480/';
+
+let wikiAPI = '';
+
+let loremWord = 'paris';
+
+let loremRando = '?random=1';
+
+// get the word from the input field
+
+function getTheLoremImage() {
+  let theInputValue = inputOne.value;
+  let newImageURL = `${loremFlickr}${theInputValue}${loremRando}`;
+  return newImageURL;
+}
+
+function updateImgURL() {
+  let newURL = getTheLoremImage();
+  console.log('updateIMG url is being fired');
+  imageContainer.src = newURL;
+}
+
+//
+function useXHR() {
+  let xhr = new XMLHttpRequest();
+  // console.log(xhr);
+
+  xhr.open('GET', getTheLoremImage());
+  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+  xhr.onprogress = () => {
+    console.log('ready state:', xhr.readyState);
+    imageContainer.innerHTML = `${loader}`;
+  };
+
+  xhr.onload = () => {
+    console.log('ready state:', xhr.readyState);
+    if (this.status == 200) {
+      imageContainer.src = this.response;
+    }
+  };
+  xhr.onerror = function() {
+    console.log('there was an error making the request: ');
+  };
+  xhr.send();
+  // if (request.status === 200) {
+  //   console.log(request.responseText);
+  // }
+  console.log('button is being pressed');
+}
+
+buttonOne.addEventListener('click', useXHR);
