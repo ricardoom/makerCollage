b609d6ebe84ba30f2ebe65d3a4b5cb6247e1c47c
Ricardo E. Galvez <galvez667@gmail.com>
2019-11-22 12:17:21 -0500

    :meat_on_bone: a few things some dynamic circle manipulation on button press sorted - TODO:  * could do better w/ the function calls, probably a way to use a higher order function  * set up translation service via Yandex so the english word entered is returned in Arabic, or what not b/c those languages are much prettier than english...   - also investigate how to getall manipulations under on button / key press * more shape generation / manipulation / masking   - look into filter effects

--- app/index.js
+++ app/index.js
@@ -1,38 +1,70 @@
 // collage maker
 
 // place images on screen
-const imageContainer = document.getElementById("firstImage");
+const primaryContainer = document.querySelector('.primary-container');
 
-const buttonOne = document.getElementById("buttonOne");
+const imageContainer = document.getElementById('firstImage');
 
-const inputOne = document.getElementById("inputFieldOne");
+const textImageContainer = document.getElementById('textImage');
+
+const buttonOne = document.getElementById('buttonOne');
+
+const inputOne = document.getElementById('inputFieldOne');
 
 // SVG elements
-const bigWordSVG = document.getElementById("textClipper");
-let bigWordWord = document.getElementById("firstWord");
+const bigWordSVG = document.getElementById('textClipper');
 
+let bigWordWord = document.getElementById('firstWord');
+
+// const circles = document.getElementsByTagName('circle');
+const circles = document.querySelectorAll('circle');
 // XHR Messages
 const message = {
-  loading: "<span>Loading Cabron...</span>",
-  errors: "<span>Error Pendejo...</span>",
-  loaded: "<span>Your new image</span"
+  loading: '<span>Loading Cabron...</span>',
+  errors: '<span>Error Pendejo...</span>',
+  loaded: '<span>Your new image</span'
 };
 
-const outputContainer = document.getElementById("output");
+const outputContainer = document.getElementById('output');
 
-const loadingSpinner = "assets/Spinner-Loading.gif";
+const loadingSpinner = 'assets/Spinner-Loading.gif';
 
+//
+// Dynamic SVG manipulations:
+//
+// move the circles around dynamically, on refresh / or on button press
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function dynamicCircles() {
+  console.log('dynamicCircles getting called');
+
+  const x = primaryContainer.offsetWidth;
+
+  const y = primaryContainer.offsetHeight;
+
+  const r = 150;
+
+  circles.forEach(circ => circ.setAttribute('cx', getRandomInt(x)));
+  circles.forEach(circ => circ.setAttribute('cy', getRandomInt(y)));
+  circles.forEach(circ => circ.setAttribute('r', getRandomInt(r)));
+}
+
+//
 // Use only images from lorem flickr
-let loremFlickr = "https://loremflickr.com/640/480/";
+//
+let loremFlickr = 'https://loremflickr.com/640/480/';
 
-let loremWord = "paris";
+let loremWord = 'paris';
 
-let loremRando = "?random=1";
+let loremRando = '?random=1';
 
 // get the word from the input field
 function getTheLoremImage() {
   let theInputValue = inputOne.value;
-  let newImageURL = `${loremFlickr}${theInputValue}${loremRando}`;
+  const newImageURL = `${loremFlickr}${theInputValue}${loremRando}`;
   return newImageURL;
 }
 
@@ -42,51 +74,46 @@ function getTheLoremImage() {
 //   return theURL;
 // }
 
-// change the big word based on input
-function changeWord() {}
-
 // request images from  lorem flickr
 function useXHR() {
-  console.log("the button was pressed");
-  let xhr = new XMLHttpRequest();
+  console.log('the button was pressed');
+  const xhr = new XMLHttpRequest();
 
   console.log(xhr);
 
-  xhr.open("GET", getTheLoremImage());
-  //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  xhr.open('GET', getTheLoremImage());
 
   xhr.onprogress = () => {
-    console.log("On Progress ready state:", xhr.readyState);
+    console.log('On Progress ready state:', xhr.readyState);
     outputContainer.innerHTML = `${message.loading}`;
     imageContainer.src = loadingSpinner;
-    outputContainer.setAttribute("class", "loading");
+    textImageContainer.src = loadingSpinner;
+    outputContainer.setAttribute('class', 'loading');
   };
-
-  // xhr.onreadystatechange = () => {
-  //   if (xhr.readyState === 4 && xhr.status === 200) {
-  //     outputContainer.setAttribute('class', 'loaded');
-  //   }
-  // };
 
   xhr.onload = () => {
     console.log(
-      "Onload Ready State is:",
+      'Onload Ready State is:',
       xhr.readyState,
-      "On Load xhr responseURL is: ",
+      'On Load xhr responseURL is: ',
       xhr.responseURL
     );
+
     imageContainer.src = xhr.responseURL;
+    textImageContainer.src = xhr.responseURL;
     outputContainer.innerHTML = `${message.loaded}`;
-    outputContainer.setAttribute("class", "loaded");
+    outputContainer.setAttribute('class', 'loaded');
     bigWordWord.innerHTML = inputOne.value;
+    dynamicCircles();
   };
 
   xhr.onerror = () => {
-    console.log("there was an error making the request: ");
+    console.log('there was an error making the request: ');
     outputContainer.innerHTML = `${message.errors}`;
   };
 
   xhr.send();
 }
 
-buttonOne.addEventListener("click", useXHR);
+// Do the things:
+buttonOne.addEventListener('click', useXHR);
